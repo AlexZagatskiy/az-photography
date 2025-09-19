75787d5be413782bd05a74e661497818fa74d3e8
Oleksii <alexzagatskiy@gmail.com>
2025-09-19 21:02:33 +0200

    fix image loader directive

--- src/app/directives/imageSrc.ts
+++ src/app/directives/imageSrc.ts
@@ -1,18 +1,24 @@
-import { Directive, ElementRef, inject, input, OnInit } from "@angular/core";
+import { Directive, effect, ElementRef, inject, input, OnInit } from "@angular/core";
 
 @Directive({
   selector: "[imgSrc]",
   standalone: true
 })
-export class ImageSrcDirective implements OnInit {
+export class ImageSrcDirective  {
   private elementRef = inject(ElementRef);
   public readonly imgSrc = input.required<string>();
 
-  ngOnInit() {
-    const img = new Image();
-    img.onload = () => {
-      this.elementRef.nativeElement.src = this.imgSrc();
-    }
-    img.src = this.imgSrc();
+  constructor() {
+    effect(() => {
+      const imgSrc = this.imgSrc();
+      if (!imgSrc) {
+        return;
+      }
+      const img = new Image();
+      img.onload = () => {
+        this.elementRef.nativeElement.src = imgSrc;
+      }
+      img.src = imgSrc;
+    });
   }
 }
